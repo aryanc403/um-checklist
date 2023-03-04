41a22900471169c112433dc6ef0f4e4299491876
Aryan <github@aryanc403.com>
2023-03-04 22:57:30 +0530

    (fix) imports

--- my-app/src/components/problems/Problem.tsx
+++ my-app/src/components/problems/Problem.tsx
@@ -1,8 +1,9 @@
-import * as React from 'react';
-import { problemsDataT } from './Context';
-import { ProblemStatus } from '../storage/Context';
-import { green, blue, blueGrey, indigo } from '@mui/material/colors';
-import { ProblemContext } from './Context';
+import * as React from "react";
+import { problemsDataT } from "./Context";
+import { ProblemStatus } from "../storage/Context";
+import { green, blue, blueGrey, indigo } from "@mui/material/colors";
+import { ProblemContext } from "./Context";
+import { TableRow, TableCell, Button } from "@mui/material";
 
 export const ProblemRow: React.FunctionComponent<problemsDataT> = ({
   episode,
@@ -20,28 +21,28 @@ export const ProblemRow: React.FunctionComponent<problemsDataT> = ({
 
   const backgroundColor = React.useMemo(() => {
     switch (status) {
-    case ProblemStatus.SOLVED:
-      return green.A200;
-    case ProblemStatus.TRYING:
-      return blue.A100;
-    case ProblemStatus.UNSOLVED:
-      return indigo[50];
-    case ProblemStatus.SKIP:
-      return blueGrey.A200;
+      case ProblemStatus.SOLVED:
+        return green.A200;
+      case ProblemStatus.TRYING:
+        return blue.A100;
+      case ProblemStatus.UNSOLVED:
+        return indigo[50];
+      case ProblemStatus.SKIP:
+        return blueGrey.A200;
     }
   }, [status]);
 
   const getNewStatus = React.useCallback(
     (status: ProblemStatus): ProblemStatus => {
       switch (status) {
-      case ProblemStatus.UNSOLVED:
-        return ProblemStatus.TRYING;
-      case ProblemStatus.TRYING:
-        return ProblemStatus.SOLVED;
-      case ProblemStatus.SOLVED:
-        return ProblemStatus.SKIP;
-      case ProblemStatus.SKIP:
-        return ProblemStatus.UNSOLVED;
+        case ProblemStatus.UNSOLVED:
+          return ProblemStatus.TRYING;
+        case ProblemStatus.TRYING:
+          return ProblemStatus.SOLVED;
+        case ProblemStatus.SOLVED:
+          return ProblemStatus.SKIP;
+        case ProblemStatus.SKIP:
+          return ProblemStatus.UNSOLVED;
       }
     },
     []
--- my-app/src/components/problems/Table.tsx
+++ my-app/src/components/problems/Table.tsx
@@ -1,13 +1,15 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { ProblemContext } from './Context';
-import { ProblemRow } from './Problem';
+import * as React from "react";
+import {
+  TableContainer,
+  TableCell,
+  TableBody,
+  Table,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import { ProblemContext } from "./Context";
+import { ProblemRow } from "./Problem";
 
 export const ProblemsTable: React.FunctionComponent<{}> = () => {
   const { problemsData } = React.useContext(ProblemContext);
